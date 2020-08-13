e6353275984e742bbaa4dce7ea77c3f72a914b9e
Pham Tien Thanh <vicnoob@MacBook-Pro-cua-Pham.local>
2020-08-13 11:03:50 +0700

    test

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var express = require("express");
 var app = express();
-app.listen(3000, () => {
+var cors = require('cors')
+
+app.use(cors())
+app.listen(8080, () => {
  console.log("Server running on port 3000");
+});
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 app.get("/url", (req, res, next) => {
   res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+});
+
+app.get("/", (req, res, next) => {
+  res.json(["Hi","Bu","From","ViC"]);
 });
